3b3d512e550af60bc7eae8451e905c697838e55a
Alex Foley <foleyfactor98@gmail.com>
2016-05-13 13:17:57 -0400

    Changed the servo and main programs
    The angling is a bit better now. Next week we're going to check if it
    can find a ball in a ring.

--- main.py
+++ main.py
@@ -25,17 +25,19 @@ while True:
 
 			#If the ball is off center left, spin that way
 			if (x < cameraThread.width/2 - 25):
-				motorDriver.right(True)
-				motorDriver.left(False)
-
-			#If the ball is off center right, spin that way
-			elif (x > cameraThread.width/2 + 25):
 				motorDriver.right(False)
 				motorDriver.left(True)
 
+			#If the ball is off center right, spin that way
+			elif (x > cameraThread.width/2 + 25):
+				motorDriver.right(True)
+				motorDriver.left(False)
+
 			# For now, if the ball is centered, charge it head on
 			else:
-				servo.setServoPos(theta, 10)
+				if abs(prevAngle - theta) > 2:
+					servo.setServoPos(theta, 2)
+					prevAngle = theta
 				motorDriver.forward()
 
 		else:
