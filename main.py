c281de43e5b177195f6e12aeb725f93de3fd46e3
Alex Foley <foleyfactor98@gmail.com>
2016-05-24 14:08:15 -0400

    Another Attempted Speed Fix
    Let's try this...

--- main.py
+++ main.py
@@ -8,7 +8,16 @@ import servo
 cameraThread = ball_tracking.BallTracker()
 cameraThread.start()
 
+FPS = 60
+delay = 1/FPS
+pctOn = 0.5
+
 prevAngle = 0
+
+def reduceSpeed():
+	time.sleep(pctOn*delay)
+	motorDriver.motorsOff()
+	time.sleep((1-pctOn)*delay)
 
 #Loop a couple of times
 while True:
@@ -24,14 +33,16 @@ while True:
 			theta = info[3]
 
 			#If the ball is off center left, spin that way
-			if (x < cameraThread.width/2 - 75):
+			if (x < (cameraThread.width/2 - 100)):
 				motorDriver.right(False)
 				motorDriver.left(True)
+				reduceSpeed()
 
 			#If the ball is off center right, spin that way
-			elif (x > cameraThread.width/2 + 75):
+			elif (x > (cameraThread.width/2 + 100)):
 				motorDriver.right(True)
 				motorDriver.left(False)
+				reduceSpeed()
 
 			# For now, if the ball is centered, charge it head on
 			else:
@@ -42,7 +53,9 @@ while True:
 					servo.setServoPos(prevAngle + 4, 10)
 					prevAngle += 4
 				motorDriver.forward()
+				reduceSpeed()
 				
 		else:
 			motorDriver.left(True)
 			motorDriver.right(False)
+			reduceSpeed()
